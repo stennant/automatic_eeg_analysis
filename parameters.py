bb062a5a96363636338fdd3f4d98c84c96ab68e0
Sarah Tennant <stennan2@exseed.ed.ac.uk>
2024-02-26 14:38:07 +0000

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -21,68 +21,68 @@ class Parameters:
 
 
     def get_is_ubuntu(self):
-        return parameters.is_ubuntu
+        return Parameters.is_ubuntu
 
     def set_is_ubuntu(self, is_ub):
-        parameters.is_ubuntu = is_ub
+        Parameters.is_ubuntu = is_ub
 
     def get_is_windows(self):
-        return parameters.is_windows
+        return Parameters.is_windows
 
     def set_is_windows(self, is_win):
-        parameters.is_windows = is_win
+        Parameters.is_windows = is_win
 
     def get_sampling_rate(self):
-        return parameters.sampling_rate
+        return Parameters.sampling_rate
 
     def set_sampling_rate(self, sr):
-        parameters.sampling_rate = sr
+        Parameters.sampling_rate = sr
 
     def get_local_recording_folder_path(self):
-        return parameters.local_recording_folder_path
+        return Parameters.local_recording_folder_path
 
     def set_local_recording_folder_path(self, path):
-        parameters.local_recording_folder_path = path
+        Parameters.local_recording_folder_path = path
 
     def get_file_path(self):
-        return parameters.file_path
+        return Parameters.file_path
 
     def set_file_path(self, path):
-        parameters.file_path = path
+        Parameters.file_path = path
 
     def get_output_path(self):
-        return parameters.output_path
+        return Parameters.output_path
 
     def set_output_path(self, path):
-        parameters.output_path = path
+        Parameters.output_path = path
 
     def get_number_of_channels(self):
-        return parameters.number_of_channels
+        return Parameters.number_of_channels
 
     def set_number_of_channels(self, length):
-        parameters.number_of_channels = length
+        Parameters.number_of_channels = length
 
     def get_sample_datatype(self):
-        return parameters.sample_datatype
+        return Parameters.sample_datatype
 
     def set_sample_datatype(self, in6):
-        parameters.sample_datatype = in6
+        Parameters.sample_datatype = in6
 
 
     def get_display_decimation(self):
-        return parameters.display_decimation
+        return Parameters.display_decimation
 
     def set_display_decimation(self, in6):
-        parameters.display_decimation = in6
+        Parameters.display_decimation = in6
 
     def get_start_sample(self):
-        return parameters.start_sample
+        return Parameters.start_sample
 
     def set_start_sample(self, in6):
-        parameters.start_sample = in6
+        Parameters.start_sample = in6
 
     def get_end_sample(self):
-        return parameters.end_sample
+        return Parameters.end_sample
 
     def set_end_sample(self, in6):
-        parameters.end_sample = in6
+        Parameters.end_sample = in6
