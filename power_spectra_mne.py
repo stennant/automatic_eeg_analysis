21cc95b8b8d0f0256738ba2b19174181b30f96de
Sarah Tennant <stennan2@exseed.ed.ac.uk>
2024-03-15 13:18:18 +0000

    Update power_spectra_mne.py

--- power_spectra_mne.py
+++ power_spectra_mne.py
@@ -31,7 +31,8 @@ display_decimation = 1
 
 #To set start and end times, put sample start and end below
 start_sample=15054049
-end_sample=36688608
+end_sample=16688608
+#end_sample=36688608
 tmin = start_sample/sample_rate
 tmax = end_sample/sample_rate
 
@@ -44,6 +45,13 @@ file_name = file_path + recording
 
 # path to the sleep score output from R below
 state_data_path = '/Users/sarahtennant/Work_Alfredo/Analysis/EOUBE/OUTPUT/EOUBE/EOUBE_454redo/EOUBE_1045_BL1-dge_swd.csv'
+
+
+colors=dict(mag='darkblue', grad='b', eeg='k', eog='k', ecg='m',
+
+      emg='g', ref_meg='steelblue', misc='steelblue', stim='b',
+
+     resp='k', chpi='k')
 
 
 # load data file as RawArray using mne package
@@ -76,23 +84,23 @@ def load_dat(file_name):
 
     # For testing code, take only a small segment of the data
     #custom_raw = custom_raw.crop(tmin=0, tmax=60) # this is for testing
-    #custom_raw.crop(tmin=0, tmax=60).plot()
+    custom_raw = custom_raw.crop(tmin, tmax)
     return custom_raw
 
 
 # Create a label for the sleep data - currently unused
 def label_sleep_data(data):
-    data['event'] = ''
+    data['description'] = ''
 
     # Define a function to map the values
     def set_value(row_number, assigned_value):
         return assigned_value[row_number]
 
     # Create the dictionary
-    event_dictionary = {1: "Wake", 2: "NREM", 0: "REM"}
+    event_dictionary = {0: "Wake", 1: "NREM", 2: "REM", 4: "SWD"}
 
     # Add a new column named 'Price'
-    data['event'] = data['sleep.score'].apply(set_value, args=(event_dictionary, ))
+    data['description'] = data['sleep.score'].apply(set_value, args=(event_dictionary, ))
     return data
 
 
@@ -101,31 +109,50 @@ def load_state_data(state_data_path):
 
     # Load sleep state score from .csv
     data = pd.read_csv(state_data_path, delimiter=",") # read .csv file with sleep score
-    data['time'] = data.index # make column of time (index of the dataframe)
-    data['duration'] = 5 # this column is needed for mne.Epochs function
+    data['onset'] = data.index/5 # make column of time (index of the dataframe)
+    data['duration'] = 5 # set duration of event - set as 5 since the sleep score is calculated in epochs of 5 seconds
 
-    data = data[['time', 'duration', 'sleep.score']] # Take only these columns and in the order that is appropraite for mne.Epochs function [time stamp, duration, event label]
-    return data
+    data = label_sleep_data(data)
+
+    # Get sleep data in correct format for Epoch function - array of nested lists
+    annotations = mne.Annotations(onset = np.asarray(data["onset"]), duration = np.asarray(data["duration"]),description = np.asarray(data["description"]))
+
+    return annotations
+
+
+def view_annotations(custom_raw, events ):
+    event_id = {"Wake":0, "NREM":1, "REM":2, "SWD":4}
+    annotated_custom_raw = custom_raw.copy().set_annotations(events)
+    events = mne.events_from_annotations(annotated_custom_raw, event_id = event_id )
+    array_events = list(events[0])
+    return annotated_custom_raw, array_events
 
 
 # calculate power spectra and plot
-def power_spectra(custom_raw, state_data):
+def power_spectra_by_state(custom_raw, annotated_custom_raw, array_events, annotations):
 
     # calculate the power spectra for all data using method = "welchs"
-    custom_raw.compute_psd(method="welch", remove_dc = "True", average = False).plot(picks='eeg', average = False, spatial_colors = True)
+    #custom_raw.compute_psd(method="welch", remove_dc = "True", average = False).plot(picks='eeg', average = False, spatial_colors = True)
 
-    # Get sleep data in correct format for Epoch function - array of nested lists
-    state_data_for_epochs = list(state_data.itertuples(index=False, name=None))
+    # Assign labels to sleep score
+    event_id = {"Wake":0, "NREM":1, "REM":2, "SWD":4}
 
     # Get epochs for just wake times
-    epoch_data = mne.Epochs(raw = custom_raw, events = state_data_for_epochs, event_id = dict(Wake=0, NREM =1, REM=2, SWD=4))
+    epoch_data = mne.Epochs(raw = annotated_custom_raw, events = array_events, event_id = event_id, tmin=-0.1, tmax = 4.9)
+    #epoch_data = mne.Epochs(raw = custom_raw, events = state_data, event_id = event_id)
     #epoch_data.plot(picks = ['eeg', 'emg']) # plot epoch data
+    event_color_dictionary = {0: "green", 1: "red", 2: "blue", 4: "black"}
+
+    epoch_data.plot(picks = 'eeg' , n_epochs = 4, scalings ="auto")
+
+    # Visualise raw epoched data with events
+    epoch_data.compute_psd(method="welch", picks='eeg', average=False, amplitude=False, exclude="bads").plot(picks = 'eeg', spatial_colors=True)
 
     # Subset data by sleep state
-    psd_swd = epoch_data['SWD'].compute_psd(method="welch")
-    psd_rem = epoch_data['REM'].compute_psd(method="welch")
-    psd_nrem = epoch_data['NREM'].compute_psd(method="welch")
-    psd_wake = epoch_data['Wake'].compute_psd(method="welch")
+    psd_swd = epoch_data['SWD'].compute_psd(method="welch", amplitude=False, exclude="bads")
+    psd_rem = epoch_data['REM'].compute_psd(method="welch", amplitude=False, exclude="bads")
+    psd_nrem = epoch_data['NREM'].compute_psd(method="welch", amplitude=False, exclude="bads")
+    psd_wake = epoch_data['Wake'].compute_psd(method="welch", amplitude=False, exclude="bads")
 
     # plot power spectra for wake
     axes = plt.subplot() # create your own axes object
@@ -142,6 +169,8 @@ def power_spectra(custom_raw, state_data):
 
 custom_raw = load_dat(file_name)
 
-state_data = load_state_data(state_data_path)
+annotations = load_state_data(state_data_path)
 
-power_spectra(custom_raw, state_data)
+annotated_custom_raw,events = view_annotations(custom_raw, annotations)
+
+power_spectra_by_state(custom_raw,annotated_custom_raw, events, annotations)
